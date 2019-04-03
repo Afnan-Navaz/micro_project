1b352dae91d1107e797d68a68b5f1c9bab0155de
Afnan-Navaz <afnannavaz@gmail.com>
2019-04-03 18:09:02 +0530

    k

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ let sub = document.querySelector('#sub');
 let cont = document.querySelector('#cont');
 let bac = document.querySelector('#back');
 let wid = window.innerWidth;
-document.querySelector('#color').style.height = window.innerHeight + "px";
 let x = 61;
 let y = Math.ceil(x/6);
 let i = 1;
 let clas = "x";
-let tTable = {CS2A:{sub:["differential","design","chemistry","computer","electronics","mechanics"]},CS2B:{sub:["differential","design","chemistry","computer","electronics","mechanics"]},EEE2:{sub:["design","graphics","electronics","differential","mechanics","physics"]},EBE2:{sub:["mechanics","design","chemistry","electrical","differential","mechanics"]},EC2A:{sub:["design","graphics","civil","electrical","differential","physics"]},EC2B:{sub:["design","graphics","civil","electrical","differential","physics"]},CS4A:{},CS4B:{},EE4:{},EB4:{},EC4A:{},EC4B:{},CS6A:{},CS6B:{},EE6:{},EB6:{},EC6A:{},EC6B:{},CS8A:{},CS8B:{},EE8:{},EB8:{},EC8A:{},EC8B:{}};
+let tTable = {CS2A:{sub:["BEE","CP","CHE","DEQ","DSE","EM"]},CS2B:{sub:["BEE","CP","CHE","DEQ","DSE","EM"]},EEE2:{sub:["BME","BEL","PHY","EGR","DEQ","DSE"]},EBE2:{sub:["CHE","EM","BME","DEQ","DSE","BEE"]},EC2A:{sub:["DEQ","BEE","PHY","CVL","DSE","EGR"]},EC2B:{sub:["DEQ","BEE","PHY","CVL","DSE","EGR"]},CS4A:{sub:["PDD","PNM","OS","COA","OOD","BE"]},CS4B:{sub:["PDD","PNM","OS","COA","OOD","BE"]},EEE4:{sub:["PNM","DE","EM","MS","SIM","LS"]},EBE4:{sub:["MC","FCP","PNM","ICS","BP","BE"]},EC4A:{sub:["PRP","ACE","CO","SS","LS","AIC"]},EC4B:{sub:["PRP","ACE","CO","SS","LS","AIC"]},CS6A:{sub:["CN","CD","SE","POM","DAA","CE"]},CS6B:{sub:["CN","CD","SE","POM","DAA","CE"]},EEE6:{sub:["EMT","ACT","PSA","POM","ED","CE"]},EBE6:{sub:["CMB","ADE","CSE","BSPI","LSD","CE"]},EC6A:{sub:["AWP","ES","VLSI","CE","DC","OOP"]},EC6B:{sub:["AWP","ES","VLSI","CE","DC","OOP"]},CS8A:{sub:[""]},CS8B:{},EEE8:{},EBE8:{},EC8A:{},EC8B:{}};
+
+document.querySelector('#color').style.height = window.innerHeight + "px";
 
 setTimeout(()=>{
     document.querySelector('#first').style.display ="block";
     document.querySelector('#zero').style.display = "none";
 },2000);
+
 cont.addEventListener('click', function(e){
     clas = document.querySelector('#bat').value+document.querySelector('#sem').value+document.querySelector('#div').value;
     if(clas.length == 4){
